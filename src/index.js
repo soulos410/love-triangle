8d38115e0f353f46f9f533396fbf0ed4a453ceb6
Siarhei <solimr111@mail.ru>
2018-02-20 00:20:05 +0300

    completed task 2

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
 let counter = 0;
 preferences.unshift(0);
 
-for(let i = 1; i < preferences.length; ){
-	let first_index = preferences[i];
-	let second_index = preferences[first_index];
-	let third_index = preferences[second_index];
-	if(i === third_index){
-		counter++;		
+for(let i = 1; i < preferences.length; i++){
+	let firstIndex = preferences[i];
+	let secondIndex = preferences[firstIndex];
+	let thirdIndex = preferences[secondIndex];
+	if(preferences[i] === 0 || preferences[firstIndex] === preferences[secondIndex]) { continue;}
+	if(i === thirdIndex){
+		counter++;
+		preferences[firstIndex] = 0;
+		preferences[secondIndex] = 0;
+		preferences[thirdIndex] = 0;		
 	}
-	i+=3;
-
 }
 	return counter;
 }
-//getLoveTrianglesCount([2,3,1,5,6,4]);
